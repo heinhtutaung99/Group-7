a2df0f30e9e9795b7a5e50d32ee9b441249ec929
Myat Thuta <40414293@live.napier.ac.uk>
2019-08-22 09:22:03 +0630

    first step for integration testing.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -12,12 +12,12 @@ public class App
     /**
      * Connect to the MySQL database.
      */
-    public void connect()
+    public void connect(String location)
     {
         try
         {
             // Load Database driver
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch (ClassNotFoundException e)
         {
@@ -25,7 +25,7 @@ public class App
             System.exit(-1);
         }
 
-        int retries = 20;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -34,13 +34,13 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "pass");
+                con = DriverManager.getConnection("jdbc:mysql://" + location + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "pass");
                 System.out.println("Successfully connected");
                 break;
             }
             catch (SQLException sqle)
             {
-                System.out.println("Failed to connect to database attempt " + i);
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
             }
             catch (InterruptedException ie)
@@ -208,7 +208,7 @@ public class App
         // Create new Application
         App a = new App();
         // Connect to database
-        a.connect();
+        a.connect("localhost:33060");
         //Country arraylist
         //ArrayList<Country> countries=a.getReportOne();
         //a.printCountryReport(countries);
