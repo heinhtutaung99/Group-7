b50df6a94865a2ec663a7d027f48d55fb86895cf
Myat Thuta <40414293@live.napier.ac.uk>
2019-08-23 13:22:31 +0630

    Updating world.java file.

--- src/main/java/com/napier/sem/places/World.java
+++ src/main/java/com/napier/sem/places/World.java
@@ -4,14 +4,16 @@ import com.napier.sem.utils.Population;
 
 import java.util.ArrayList;
 /**
- Stores information about the attributes of the world.
+ Stores information about the attributes of the world
  */
 public class World implements Population {
 
     private World() {}
 
     private static World instance = null;
-
+    /**
+     *  Getting information with Get Method
+     * */
     public static World getInstance() {
         if (instance  == null) {
             instance = new World();
@@ -20,7 +22,9 @@ public class World implements Population {
     }
 
     private ArrayList<Continent> continents;
-
+    /**
+     * Getting Continents with Get Method
+     */
     public ArrayList<Continent> getContinents() {
         return continents;
     }
@@ -28,7 +32,9 @@ public class World implements Population {
     public void setContinents(ArrayList<Continent> continents) {
         this.continents = continents;
     }
-
+    /**
+     * Getting population with Get Method
+     */
     public long getPopulation() {
         long worldPopulation = 0;
         for (Continent continent : continents) {
