db2959160bf59c6cd21025a440946849613cef20
MinHeinHtet <40414300@live.napier.ac.uk>
2019-08-26 14:06:15 +0630

    Add Integration Test to travis.

---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppIntegrationTest {
+
+    static App app;
+    static DatabaseHandler db;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        db = DatabaseHandler.Instance();
+        db.connect("localhost:3306");
+    }
+
+
+
+    @Test
+    void testReportOne(){
+        Country r = (Country) db.getReportOne();
+        int size =r.get_reportsItems().size();
+        assertTrue(size >0);
+
+    }
+}
